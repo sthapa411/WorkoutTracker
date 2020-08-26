566fc30fa587c229ac9e3278be8638354a949ae0
sthapa <san20hal@gmail.com>
2020-08-25 21:12:14 -0400

    new heroku app

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
