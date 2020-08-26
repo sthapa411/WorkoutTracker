ee35427ec67ea7b37b247af6f04558fc1e4b842f
sthapa <san20hal@gmail.com>
2020-08-25 21:42:49 -0400

    database name fixed

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
